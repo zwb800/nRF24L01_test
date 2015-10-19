74af2115360a0d1eea7d3c6cc28ae04ee569ad13
zwb800 <zwb800@gmail.com>
2015-10-19 14:37:26 +0800

    基于ack with payload的双向通信

--- nRF24L01_test.h
+++ nRF24L01_test.h
@@ -1,25 +1,35 @@
 #define  R_REGISTER      0x00 //SPI读命令
 #define W_REGISTER      0x20 //SPI写命令
-#define  TX_ADDR       0x10 //发送地址寄存器
-#define  RX_ADDR_P0      0x0A//通道0的接收地址
-#define  EN_AA       0x01//启用自动ACK
+
+#define CONFIG 0x00//设置
+#define  EN_AA  0x01//启用自动ACK
 #define EN_RXADDR 0x02//启动接收通道
 #define SETUP_RETR 0x04//设置重发
 #define RF_CH 0x05//设置频段
 #define RF_SETUP 0x06//设置功率
-#define CONFIG 0x00//设置
+#define  STATUS        0x07//状态寄存器
+#define OBSERVE_TX  0x08//观察发送状态
+#define  RX_ADDR_P0      0x0A//通道0的接收地址
+#define  RX_ADDR_P1      0x0B//通道1的接收地址
+#define  TX_ADDR       0x10 //发送地址寄存器
+#define RX_PW_P0 0x11//通道0的数据长度
+#define RX_PW_P1 0x12//通道1的数据长度
+#define DYNPD 0x1C//启动接收动态数据
+#define FEATURE 0x1D//启用动态数据
+
 #define  W_TX_PAYLOAD    0xA0//发射数据区
-#define  W_RX_PAYLOAD    0x61//接收数据区
+#define  R_RX_PAYLOAD    0x61//接收数据区
 #define FLUSH_TX 0xE1//清空发送区
 #define  FLUSH_RX 0xE2//清空接收区
-#define  STATUS        0x07//状态寄存器
-#define RX_PW_P0 0x11//通道0的数据长度
+#define R_RX_PL_WID    0x60//接收到的数据长度
 
 #define MASK_TX_DS    0x20//发送完毕
 #define MASK_RX_DR    0x40//接收完毕
+#define MASK_MAX_RT 0x10//达到最大重发次数
 
-#define R_RX_PL_WID    0x60//接收到的数据长度
-#define DYNPD 0x1C//启动接收动态数据
-#define FEATURE 0x1D//启用动态数据
+#define W_ACK_PAYLOAD 0xA8//ACK Payload
+
+#define ACTIVATE 0x50//激活
+
 
 
--- nRF24L01_test.ino
+++ nRF24L01_test.ino
@@ -1,160 +1,189 @@
-  /* CS/SS -  pin 10
-  * MOSI - pin 11
-  * MISO - pin 12
-  * SCK - pin 13
+/* CS/SS -  pin 10
+* MOSI - pin 11
+* MISO - pin 12
+* SCK - pin 13
 */
 #include <SPI.h>
 #include  "nRF24L01_test.h"
+#define MODE_TX 0
+#define MODE_RX 1
+#define INIT_MODE MODE_RX
 
 const int slaveSelectPin = 9;//SPI片选
 const int chipEnablePin = 10;//CE 芯片开关
 
 #define TX_ADR_WIDTH 5//发射地址位长度
 
-byte TX_ADDRESS[TX_ADR_WIDTH]  = //发射地址
+byte TX_ADDRESS[TX_ADR_WIDTH]  = //发射地址 低位先写入
 {
-  0x34,0x43,0x10,0x10,0x01
+  0xe1, 0xf0, 0xf0, 0xe8, 0xe8
 };
 #define TX_PAYLOAD_WIDTH 32//发射数据长度
 #define RX_PAYLOAD_WIDTH 32//接收数据长度
 byte txBuffer[TX_PAYLOAD_WIDTH] = {0};//发射数据
 byte rxBuffer[RX_PAYLOAD_WIDTH] = {0};//接收数据
+boolean txReady  = true;
+boolean ackReady = true;
 
 void setup() {
-  pinMode(chipEnablePin,OUTPUT);
-  pinMode(slaveSelectPin,OUTPUT);
-  digitalWrite(slaveSelectPin,HIGH);//关闭SPI
+  pinMode(chipEnablePin, OUTPUT);
+  pinMode(slaveSelectPin, OUTPUT);
+  digitalWrite(slaveSelectPin, HIGH); //关闭SPI
   SPI.begin();
   Serial.begin(115200);
   Serial.setTimeout(2);
-
+  commonSetting();
+#if(INIT_MODE==MODE_TX)
+  txMode();
+#else
   rxMode();
-  Serial.println("Booted");
+#endif
+
+  Serial.println(INIT_MODE == MODE_TX ? "TX Mode" : "RX Mode");
 }
-
+ byte lenTx = 0;
 void loop() {
-  int k = 0;
-  while(true)
-  {    
-    byte sstatus = readRegister(STATUS);//读取状态
+  byte sstatus = readRegister(STATUS);//读取状态
+#if(INIT_MODE==MODE_TX)
+  //判断是否发送成功
 
-   //判断是否发送成功
-//   if(sstatus & MASK_TX_DS)
-//    {
-//        writeRegister(FLUSH_TX,0);//清空发送区
-//        while(!Serial.available())
-//        {
-//        }
-//        int len = Serial.readBytes(txBuffer,TX_PAYLOAD_WIDTH);  
-//         
-//         writeRegister(W_TX_PAYLOAD,txBuffer,len);//写入发射数据
-//         Serial.println("Sended");
-//    }
-    
-   //判断是否接收成功
-    if(sstatus & MASK_RX_DR)
+  if (txReady)
+  {
+    if (Serial.available())
     {
-        byte len = readRegister(R_RX_PL_WID);
-        if(len < 0x21)//32位和以下为正确数据
+      byte len = Serial.readBytes(txBuffer, TX_PAYLOAD_WIDTH);
+      writeRegister(FLUSH_TX, 0); //清空发送区
+      writeRegister(W_TX_PAYLOAD, txBuffer, len); //写入发射数据
+      txReady = false;
+    }
+  }
+
+  if ((sstatus & MASK_TX_DS))
+  {
+    txReady = true;
+
+  }
+
+  if (sstatus & MASK_MAX_RT)
+  {
+    txReady = true;
+  }
+
+#endif
+
+    #if(INIT_MODE==MODE_RX)
+    if(ackReady)
+    {
+         if(Serial.available())
         {
-           readRegister(W_RX_PAYLOAD,rxBuffer,len);//读取接收数据
-          Serial.write(rxBuffer,len);
+             byte len = Serial.readBytes(txBuffer,TX_PAYLOAD_WIDTH);
+              //写入ACK Payload 当更改PPP时应断电重启
+            writeRegister(W_ACK_PAYLOAD  , txBuffer, len); 
+            ackReady = false;
         }
-       
-       writeRegister(FLUSH_RX,0);//清空接收区
     }
-    
-    if(sstatus > 0x0F)
+#endif
+  //判断是否接收成功
+  if (sstatus & MASK_RX_DR)
+  {
+#if(INIT_MODE==MODE_RX)
+      ackReady = true;
+#endif
+
+    byte len = readRegister(R_RX_PL_WID);
+    if (len < 0x21) //32位和以下为正确数据
     {
-      writeConfigRegister(STATUS,sstatus);//清除RX_DR TX_DS MAX_RT中断
+      readRegister(R_RX_PAYLOAD, rxBuffer, len); //读取接收数据
+      Serial.write(rxBuffer, len);
     }
+    //    writeRegister(FLUSH_RX, 0); //清空接收区
+
+
+  }
+
+  if (sstatus > 0x0F)
+  {
+    writeConfigRegister(STATUS, sstatus); //清除RX_DR TX_DS MAX_RT中断
   }
 }
 
 void txMode()
 {
-  digitalWrite(chipEnablePin,LOW);//关闭芯片
-  writeConfigRegister(TX_ADDR,TX_ADDRESS,TX_ADR_WIDTH);//设置发送地址
-  writeConfigRegister(RX_ADDR_P0,TX_ADDRESS,TX_ADR_WIDTH);//通道0的接收地址与发送地址相同
-  writeConfigRegister(EN_AA,0x01);//启动通道0的ACK
-  writeConfigRegister(EN_RXADDR,0x01);//启动接收通道0
-  writeConfigRegister(SETUP_RETR,0x1a);//重发间隔500us 重发次数10次
-  writeConfigRegister(RF_CH,40);//频段40 2.440GHz
-  writeConfigRegister(RF_SETUP,0x0E);//0dBm  2Mbps
-  writeConfigRegister(FEATURE,0x04);//启用动态数据
-  writeConfigRegister(DYNPD,0x01);//启动通道0的动态数据
-  writeConfigRegister(CONFIG,0x0e);/*发送模式 启动电源 2位CRC 启动CRC 
+  digitalWrite(chipEnablePin, LOW); //关闭芯片
+  writeConfigRegister(CONFIG, 0x7e);/*发送模式 启动电源 2位CRC 启动CRC
   启用发送完毕 接受完毕 达到重试次数中断*/
-  writeRegister(W_TX_PAYLOAD,txBuffer,TX_PAYLOAD_WIDTH);//写入发射数据
-
-  byte enaa = readRegister(EN_AA);
-  Serial.println(enaa,BIN);
-  digitalWrite(chipEnablePin,HIGH);//开启芯片
+  digitalWrite(chipEnablePin, HIGH); //开启芯片
+  delayMicroseconds(130);
 }
 void rxMode()
 {
-  digitalWrite(chipEnablePin,LOW);//关闭芯片
-  writeConfigRegister(RX_ADDR_P0,TX_ADDRESS,TX_ADR_WIDTH);//通道0的接收地址与发送地址相同
-  writeConfigRegister(EN_AA,0x01);//启动通道0的ACK
-  writeConfigRegister(EN_RXADDR,0x01);//启动接收通道0
-  writeConfigRegister(SETUP_RETR,0x1a);//重发间隔500us 重发次数10次
-  writeConfigRegister(RF_CH,40);//频段40 2.440GHz
-  writeConfigRegister(RF_SETUP,0x0E);//0dBm  2Mbps
-  writeConfigRegister(RX_PW_P0,RX_PAYLOAD_WIDTH);//通道0的接收长度为32位
-  writeConfigRegister(FEATURE,0x04);//启用动态数据 貌似不启用也能用
-  writeConfigRegister(DYNPD,0x01);//启动通道0的动态数据
-
-  writeConfigRegister(CONFIG,0x0f);/*接收模式 启动电源 2位CRC 启动CRC 
+  digitalWrite(chipEnablePin, LOW); //关闭芯片
+  writeConfigRegister(CONFIG, 0x7f);/*接收模式 启动电源 2位CRC 启动CRC
   启用发送完毕 接受完毕 达到重试次数中断*/
-  writeRegister(W_RX_PAYLOAD,rxBuffer,RX_PAYLOAD_WIDTH);//清空接收数据
-
-  byte enaa = readRegister(EN_AA);
-  Serial.println(enaa,BIN);
-  digitalWrite(chipEnablePin,HIGH);//开启芯片
+  digitalWrite(chipEnablePin, HIGH); //开启芯片
+  delayMicroseconds(130);
 }
 
-byte writeConfigRegister(byte registerAddr,byte* datas,int len)
+void commonSetting()
 {
-  return writeRegister(W_REGISTER + registerAddr,datas,len);
+   writeConfigRegister(TX_ADDR, TX_ADDRESS, TX_ADR_WIDTH); //设置发送地址
+  writeConfigRegister(RX_ADDR_P0, TX_ADDRESS, TX_ADR_WIDTH); //通道0的接收地址与发送地址相同
+  writeConfigRegister(RX_PW_P0, RX_PAYLOAD_WIDTH); //通道0的接收长度为32位
+  writeConfigRegister(EN_AA, 0x01); //启动通道0的ACK
+  writeConfigRegister(EN_RXADDR, 0x01); //启动接收通道0
+  writeConfigRegister(SETUP_RETR, 0xff); //重发间隔4000us 重发15次
+  writeConfigRegister(RF_CH, 0x7d); //2.525GHz
+  writeConfigRegister(RF_SETUP, 0x27); //0dBm(SI24R1 7dbm)  250kbps
+  writeConfigRegister(FEATURE, 0x06); //启用动态数据 ACK Payload
+  writeConfigRegister(DYNPD, 0x01); //启动通道0的动态数据
+  writeRegister(W_TX_PAYLOAD, txBuffer, TX_PAYLOAD_WIDTH); //清空发射数据
+  writeRegister(R_RX_PAYLOAD, rxBuffer, RX_PAYLOAD_WIDTH); //清空接收数据
 }
 
-byte writeConfigRegister(byte registerAddr,byte data)
+byte writeConfigRegister(byte registerAddr, byte* datas, int len)
 {
-   return writeConfigRegister(registerAddr,&data,1);
+  return writeRegister(W_REGISTER + registerAddr, datas, len);
 }
 
-byte writeRegister(byte registerAddr,byte* datas,int len)
+byte writeConfigRegister(byte registerAddr, byte data)
 {
-   digitalWrite(slaveSelectPin,LOW);//选中芯片
-   byte sstatus = SPI.transfer(registerAddr);//选择寄存器地址
-   for(int i=0;i<len;i++)
-   {
-      SPI.transfer(*datas ++);//写入
-    }
-   digitalWrite(slaveSelectPin,HIGH);//取消选中
-   return sstatus;
- }
+  return writeConfigRegister(registerAddr, &data, 1);
+}
 
-byte writeRegister(byte registerAddr,byte data)
+byte writeRegister(byte registerAddr, byte* datas, int len)
 {
-   return writeRegister(registerAddr,&data,1);
+  digitalWrite(slaveSelectPin, LOW); //选中芯片
+  byte sstatus = SPI.transfer(registerAddr);//选择寄存器地址
+  for (int i = 0; i < len; i++)
+  {
+    SPI.transfer(*datas ++);//写入
+  }
+  digitalWrite(slaveSelectPin, HIGH); //取消选中
+  return sstatus;
+}
+
+byte writeRegister(byte registerAddr, byte data)
+{
+  return writeRegister(registerAddr, &data, 1);
 }
 
 byte readRegister(byte registerAddr)
 {
-    byte data = 0;
-    readRegister(registerAddr,&data,1);
-    return data;
+  byte data = 0;
+  readRegister(registerAddr, &data, 1);
+  return data;
 }
 
-byte readRegister(byte registerAddr,byte* datas,byte len)
+byte readRegister(byte registerAddr, byte* datas, byte len)
 {
-   digitalWrite(slaveSelectPin,LOW);//选中芯片
-   byte sstatus = SPI.transfer(registerAddr);//寄存器地址 R_REGISTER为0 加不加都行 
-   for(int i=0;i<len;i++)
-   {
-      datas[i] = SPI.transfer(0);//读取
-    }
-   digitalWrite(slaveSelectPin,HIGH);//取消选中
-   return sstatus;
+  digitalWrite(slaveSelectPin, LOW); //选中芯片
+  byte sstatus = SPI.transfer(registerAddr);//寄存器地址 R_REGISTER为0 加不加都行
+  for (int i = 0; i < len; i++)
+  {
+    datas[i] = SPI.transfer(0xFF);//读取
+  }
+  digitalWrite(slaveSelectPin, HIGH); //取消选中
+  return sstatus;
 }
+
+
